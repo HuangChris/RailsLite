110b2242f25b628ed3c9dcaaf53c5857e7d9337a
Christopher Huang <CCHuang90@gmail.com>
2015-09-22 15:06:51 -0700

    phase 4

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,9 +10,20 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = {}
+      @params.merge!(route_params)
+      p req.body
+      query_data = Hash.new
+      if req.query_string
+        query = URI.decode_www_form(req.query_string)
+        query.each { |set| query_data[set[0]] = set[1] }
+      end
+      @params.merge!(query_data)
     end
 
     def [](key)
+      key = key.to_s if key.is_a?(Symbol)
+      @params[key]
     end
 
     # this will be useful if we want to `puts params` in the server log
