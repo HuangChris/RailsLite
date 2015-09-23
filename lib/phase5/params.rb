49d6fd719551db574317fc3a055a43fb2a96b86b
Christopher Huang <CCHuang90@gmail.com>
2015-09-22 18:00:21 -0700

    phase 5, Route of phase 6

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -12,18 +12,12 @@ module Phase5
     def initialize(req, route_params = {})
       @params = {}
       @params.merge!(route_params)
-      p req.body
-      query_data = Hash.new
-      if req.query_string
-        query = URI.decode_www_form(req.query_string)
-        query.each { |set| query_data[set[0]] = set[1] }
-      end
-      @params.merge!(query_data)
+      @params.merge!(parse_www_encoded_form(req.body))
+      @params.merge!(parse_www_encoded_form(req.query_string))
     end
 
     def [](key)
-      key = key.to_s if key.is_a?(Symbol)
-      @params[key]
+      @params[key.to_s] || @params[key.to_sym]
     end
 
     # this will be useful if we want to `puts params` in the server log
@@ -40,11 +34,68 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      return {} if www_encoded_form.nil?
+      pairs = www_encoded_form.split("&")
+      hash = Hash.new
+      pairs.each do |pair|
+        key = pair.match(/^(.*)=/)[1]
+        value = pair.match(/\=(.*)/)[1]
+        keys = parse_key(key)
+      #     #this should be a deep merge, I'm only doing 1 level
+      #   assign_keys(keys, value).each do |key,val|
+      #     if hash[key].nil?
+      #       hash[key] = val
+      #     else
+      #       hash[key].merge!(val)
+      #     end
+      #   end
+        hash.deep_merge!(assign_keys(keys, value))
+        # puts "merged hash #{hash}"
+      end
+      hash
+    end
+
+    def assign_keys(keys, value)
+      hash = Hash.new
+      if keys.length == 1
+        hash[keys.first] = value
+      else
+        hash[keys.shift] = assign_keys(keys, value)
+      end
+      hash
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      keys = key.split /\[|\]\[|\]/
     end
   end
 end
+
+class Hash
+  def deep_merge!(hash)
+    hash.each do |key, value|
+      if self[key].nil?
+        self[key] = value
+      else
+        self[key].deep_merge!(value)
+      end
+    end
+  end
+  self
+end
+
+#all this can be done iteratively once you have the keys and value
+# data = [[[key1, key2, key3], value], [[key1, key2, key4], value2]
+# params = {}
+# data.each do |pair|
+#   pair[0].each do |key|
+#     if key = pair[0].last
+#       params[key] = pair[1]
+#     else
+#       params[key] ||= {}
+#       params = params[key]
+#     end
+#   end
+# end
--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -3,15 +3,26 @@ module Phase6
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern = pattern
+      @http_method = http_method
+      @controller_class = controller_class
+      @action_name = action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      req.request_method.downcase.to_sym == http_method &&
+      req.path.match(pattern)
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
+      route_matches = matches?(req)
+      route_params = Hash.new
+      route_matches.names.each { |name| route_params[name] = match[name] }
+      # We need to determine route_params and replace the empty hash
+      controller_class.new(req,res, route_params).invoke_action(action_name)
     end
   end
 
@@ -19,10 +30,12 @@ module Phase6
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
@@ -37,10 +50,15 @@ module Phase6
 
     # should return the route that matches this request
     def match(req)
+      @routes.each do |route|
+        return route if route.matches?(req)
+      end
     end
 
     # either throw 404 or call run on a matched route
     def run(req, res)
+      route = match(req)
+      route ? route.run : res.status=(404)
     end
   end
 end
